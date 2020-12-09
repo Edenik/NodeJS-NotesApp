c1692c2062aeac0cc5975ad311bc65cda13b3e21
Edenik <Edenik5@gmail.com>
2020-12-09 12:36:04 +0200

    Argument Parsing with Yargs: Part 1

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
-const getNotes = require('./notes.js');
-const chalk = require('chalk');
+const getNotes = require('./notes.js')
+const chalk = require('chalk')
+const yargs = require('yargs')
 
-const command = process.argv[2];
+yargs.version('1.1.0')
 
-console.log(process.argv);
 
-if(command === 'add'){
-    console.log('Adding note!');
-} else if(command === 'remove'){
-    console.log('Removing Note!');
-}
+// create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+
+
+// create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+})
+
+
+// create read command 
+yargs.command({
+    command:'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note!')
+    }
+})
+
+// create list command 
+yargs.command({
+    command:'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('Listing the notes!')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
+
+
+// if(command === 'add'){
+//     console.log('Adding note!');
+// } else if(command === 'remove'){
+//     console.log('Removing Note!');
+// }
