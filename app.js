52b6f2181b84fe00ea2d06e83bfad4dc093bbedc
Edenik <Edenik5@gmail.com>
2020-12-09 11:46:51 +0200

    Importing npm Modules

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 // // fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
 // fs.appendFileSync('notes.txt', 'This line was appended by Node.js!')
 
-
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const notesToPrint = getNotes();
 
 console.log(notesToPrint);
+
+console.log(validator.isEmail('Edenik5@gmail.com'));
