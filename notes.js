13eae431adae04e450f5ec64450cb78f3a5da43f
Edenik <Edenik5@gmail.com>
2020-12-09 13:07:48 +0200

    Adding a note

--- notes.js
+++ notes.js
@@ -1,7 +1,42 @@
-const notes = {"note1": "hello", "note2": "bye"};
+const fs = require('fs');
 
 const getNotes = function() {
     return notes;
 }
 
-module.exports = getNotes;
+const addNote= function (title, body) {
+ const notes = loadNotes();
+ const duplicateNotes = notes.filter(note => note.title === title);
+ 
+ if(duplicateNotes.length === 0){
+    notes.push({
+        title:title,
+        body:body,
+    })
+    saveNotes(notes)
+    console.log('New note added!')
+ } else {
+    console.log('Note title taken')
+ }
+}
+
+const saveNotes = function(notes) {
+    const dataJson = JSON.stringify(notes)
+    fs.writeFileSync('data.json', dataJson)
+}
+
+const loadNotes = function(){
+    try {
+        const dataBuffer = fs.readFileSync('data.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    } catch (e){
+        return []
+    }
+
+}
+
+module.exports ={
+    getNotes:getNotes,
+    addNote:addNote
+}
